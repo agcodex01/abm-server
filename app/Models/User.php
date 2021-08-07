61b6ea94b50f045c6bb5bc11040eed4630135fcc
loremZyn14 <yasuaimo@gmail.com>
2021-08-07 09:25:09 +0800

    ABM-7: authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens, Uuids;
 
     /**
      * The attributes that are mass assignable.
@@ -17,6 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
+        'id',
         'name',
         'email',
         'password',
