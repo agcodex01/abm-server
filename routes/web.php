fe5d40f3d2c5f7c1eb1c94942d68525c352cc330
loremZyn14 <yasuaimo@gmail.com>
2021-11-27 15:46:19 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return \App\Permission\PermissionCapabilities::CAPABILITIES;
-    $roles = ['a', 'b'];
-    $access = [
-        'a' => false,
-        'b' => false
-    ];
-    foreach ($roles as $role) {
-        if ($access[$role]) {
-            return true;
-        }
-    }
-    return false;
-    // return view('welcome');
+    return view('welcome');
 });
